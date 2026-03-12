dc51b77f99dd179370ac809fcb24957b0507ae78
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2026-03-12 09:46:37 +0800

    [feat] added search query

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,7 +1,21 @@
 import { products } from "../data/products";
 import ProductCard from "../components/product/ProductCard";
+import { useState } from "react";
 
 const ProductPage = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredProduct, setFilteredProduct] = useState(products);
+
+  const handleSearchProduct = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearchQuery(value);
+    const filteredItems = products.filter((product) =>
+      product.name.toLowerCase().includes(value.toLowerCase()),
+    );
+
+    setFilteredProduct(filteredItems);
+  };
+
   return (
     <section className="px-10 py-16 max-w-7xl mx-auto">
       {/* Page Header */}
@@ -16,6 +30,8 @@ const ProductPage = () => {
       <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4 mb-10">
         {/* Search */}
         <input
+          onChange={handleSearchProduct}
+          value={searchQuery}
           type="text"
           placeholder="Search products..."
           className="w-full md:w-[300px] rounded-xl bg-white/10 border border-white/10 px-4 py-2 text-sm text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500"
@@ -33,7 +49,7 @@ const ProductPage = () => {
       {/* Product Grid */}
       {/* PRODUCT GRID */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-        {products.map((product) => (
+        {filteredProduct.map((product) => (
           <ProductCard
             key={product.id}
             product={product}
