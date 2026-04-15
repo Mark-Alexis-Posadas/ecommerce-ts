35a7529d3b2253c03719d1e73d75ccd0a18c586d
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2026-04-15 13:58:55 +0800

    update: api url

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "/api/orders";
+const API = "http://localhost:5000/api/orders";
 
 export const createOrder = async (orderData: any, token: string) => {
   return axios.post(API, orderData, {
